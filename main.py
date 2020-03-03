bed1be23665aae5a78900688b2818151d20f6fa2
Asya Khleborodova <asyakhleb@gmail.com>
2020-03-03 14:47:43 -0500

    updates

--- main.py
+++ main.py
@@ -1,29 +1,24 @@
 from sys import argv
-from sodapy import Socrata
-import os
+from src.nyc_parking_violations.parking_violations_api import get_parking_violations
 
 if __name__ =="__main__":
+	def to_json_file(file_name: str, content:list):
+		with open(file_name, "w") as res_out:
+			return res_out.write(str(content))
 
-# def get_parking_violations(page_size: int, page_num: int=None, json_file: str="stdout") -> list:
 	
-	client = Socrata("data.cityofnewyork.us", os.environ["APP_KEY"])
-
-	total = client.get("nc67-uf89", select='COUNT(*)')
-	total = int(total[0]["COUNT"])
-
-	num_pages = total/int(argv[2]) # total number of calls/number of pages
-
-	output = []
-	if argv[2]==None:
-		for i in range(num_pages): 
-			output.append(client.get("nc67-uf89", limit=int(argv[1]), offset=i*10))
-	else:
-		for i in range(int(argv[2])):
-			output.append(client.get("nc67-uf89", limit=int(argv[1]), offset=i*10))
-	print(output)
-
-	# with open(str(argv[3]), "w") as res_out:
-	# 	res_out.write(str(output))
+	if len(argv)==2:
+		get_parking_violations(int(argv[1]))
+	elif argv[2]=="results.json":
+		result = get_parking_violations(int(argv[1]))
+		to_json_file(argv[2],result)
+	elif len(argv)==3 $ int(argv[2])==int:
+		get_parking_violations(int(argv[1]), int(argv[2]))
+	elif len(argv)==4:
+		result = get_parking_violations(int(argv[1]), int(argv[2]))
+		to_json_file(argv[3],result)
 
 
+
+	
 	
---
+++ src/nyc_parking_violations/parking_violations_api.py
@@ -0,0 +1,21 @@
+
+from sodapy import Socrata
+import os
+def get_parking_violations(page_size: int, page_num: int=None) -> list:
+	
+	client = Socrata("data.cityofnewyork.us", os.environ["APP_KEY"])
+
+	total = client.get("nc67-uf89", select='COUNT(*)')
+	total = int(total[0]["COUNT"])
+
+	total_num_pages = total//page_size+1# total number of calls/page size
+
+	output = []
+	if page_num==None:
+		for i in range(total_num_pages): 
+			output.append(client.get("nc67-uf89", limit=page_size, offset=i*page_size))
+	else:
+		for i in range(int(page_num)):
+			output.append(client.get("nc67-uf89", limit=page_size, offset=i*page_size))
+
+	return(output)
