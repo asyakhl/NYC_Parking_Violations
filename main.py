b3792514cd0c24983bd6c5b0b3249840e1d35e23
Asya Khleborodova <asyakhleb@gmail.com>
2020-03-04 13:30:19 -0500

    5th commit

--- main.py
+++ main.py
@@ -1,28 +1,42 @@
 from sys import argv
 from src.nyc_parking_violations.parking_violations_api import get_parking_violations
-
+import argparse
 if __name__ == "__main__":
+
+	ap = argparse.ArgumentParser()
+	ap.add_argument("--page_size", required=True)
+	ap.add_argument("--page_num", required=False)
+	ap.add_argument("--output", required=False)
+	args = vars(ap.parse_args())
+
+	args_list = [None]*len(argv[1:])
+	for i in range(len(list(argv[1:]))):
+		indx = list(argv[1:])[i].index("=")
+		args_list[i] = ("".join(list(argv[1:])[i][:indx]))
+		
 	
 	def to_json_file(file_name: str, content:list):
 		with open(file_name, "w") as res_out:
 			for i in range(len(content)):
 				res_out.write(f"{content[i]}\n")
 			return res_out
-	try:
+	
 		if len(argv)==2:
-			print(get_parking_violations(int(argv[1])))
-		elif argv[2]=="results.json":
-			result = get_parking_violations(int(argv[1]))
-			to_json_file(argv[2],result)
+		 		print(get_parking_violations(int(args["page_size"])))
 		elif len(argv)==3:
-			print(get_parking_violations(int(argv[1]), int(argv[2])))
-		elif argv[3]=="results.json":
-			result = get_parking_violations(int(argv[1]), int(argv[2]))
-			to_json_file(argv[3],result)
-		else:
-			print("Output file name must be results.json")
-	except ValueError as e:
-	 	print(f"Use integer: {e}")
+
+			if "--output" in args_list:
+				result = get_parking_violations(int(args["page_size"]))
+				to_json_file(args["output"],result)
+			else:
+				print(get_parking_violations(int(args["page_size"]), int(args["page_num"])))
+
+		elif len(argv)==4:
+			result = get_parking_violations(int(args["page_size"]), int(args["page_num"]))
+			to_json_file(args["output"],result)
+		
+	
+	
 
 
 
